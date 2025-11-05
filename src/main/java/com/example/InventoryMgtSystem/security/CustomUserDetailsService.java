3fa72b0b7a23b45acccc2244120b3ae71c287d37
carrinson <folacarrinson@gmail.com>
2025-11-05 20:28:24 +0100

    f* security i dont get it yet

--- src/main/java/com/example/InventoryMgtSystem/security/CustomUserDetailsService.java
+++ src/main/java/com/example/InventoryMgtSystem/security/CustomUserDetailsService.java
@@ -1,13 +1,28 @@
 package com.example.InventoryMgtSystem.security;
 
 
+import com.example.InventoryMgtSystem.exceptions.NotFoundException;
+import com.example.InventoryMgtSystem.models.Users;
 import com.example.InventoryMgtSystem.repositories.UsersRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class CustomUserDetailsService {
+public class CustomUserDetailsService implements UserDetailsService {
 
-    private final UsersRepository
+    private final UsersRepository usersRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException{
+        Users user = usersRepository.findByEmail(username)
+                .orElseThrow(()-> new NotFoundException("User Email Not Found"));
+
+        return AuthUser.builder()
+                .user(user)
+                .build();
+    }
 }
