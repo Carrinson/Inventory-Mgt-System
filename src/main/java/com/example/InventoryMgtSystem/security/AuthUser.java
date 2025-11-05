4c1beb6f8e5944e665af04f399e7e2b0699b4f0a
carrinson <folacarrinson@gmail.com>
2025-11-05 11:25:43 +0100

    error except done starting security

---
+++ src/main/java/com/example/InventoryMgtSystem/security/AuthUser.java
@@ -0,0 +1,53 @@
+package com.example.InventoryMgtSystem.security;
+
+
+import com.example.InventoryMgtSystem.models.Users;
+import lombok.Builder;
+import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Data
+@Builder
+public class AuthUser implements UserDetails {
+
+    private Users user;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getPassword() {
+        return "";
+    }
+
+    @Override
+    public String getUsername() {
+        return "";
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
+}
