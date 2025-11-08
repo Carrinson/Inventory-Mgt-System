a7e6d198e015c95e2775b4b5e1662106358e3deb
carrinson <folacarrinson@gmail.com>
2025-11-09 00:00:17 +0100

    controller added

---
+++ src/main/java/com/example/InventoryMgtSystem/controllers/UserController.java
@@ -0,0 +1,45 @@
+package com.example.InventoryMgtSystem.controllers;
+
+import com.example.InventoryMgtSystem.dtos.UsersDTO;
+import com.example.InventoryMgtSystem.dtos.response.Response;
+import com.example.InventoryMgtSystem.services.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/users")
+@RequiredArgsConstructor
+public class UserController {
+
+
+    private final UserService userService;
+
+    @GetMapping("/all")
+    @PreAuthorize("hasAuthority('ADMIN')")
+    public ResponseEntity<Response> getAllUsers(){
+        return ResponseEntity.ok(userService.getAllUsers());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Response> loginUser(@PathVariable Long id){
+        return ResponseEntity.ok(userService.getUserById(id));
+    }
+
+    @GetMapping("/update/{id}")
+    public ResponseEntity<Response> updateUser(@PathVariable Long id, @RequestBody UsersDTO usersDTO){
+        return ResponseEntity.ok(userService.updateUser(id, usersDTO));
+    }
+
+    @PreAuthorize("hasAuthority('ADMIN')")
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<Response> deleteUser(@PathVariable Long id){
+        return ResponseEntity.ok(userService.deleteUser(id));
+    }
+
+    @GetMapping("/transactions/{userId}")
+    public ResponseEntity<Response> getUserAndTransactions(@PathVariable Long userId){
+        return ResponseEntity.ok(userService.getUserTransaction(userId));
+    }
+}
